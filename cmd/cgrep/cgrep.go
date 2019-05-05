f5dbf9062d5215cbe48245a3dad7b0341c4932e3
Hiroko <d.hegyesi@gmail.com>
2019-05-05 18:18:57 +0200

    Change import location to forked repository

--- cmd/cgrep/cgrep.go
+++ cmd/cgrep/cgrep.go
@@ -11,7 +11,7 @@ import (
 	"os"
 	"runtime/pprof"
 
-	"github.com/google/codesearch/regexp"
+	"github.com/Hiroko103/codesearch/regexp"
 )
 
 var usageMessage = `usage: cgrep [-c] [-h] [-i] [-l] [-n] regexp [file...]
--- regexp/match.go
+++ regexp/match.go
@@ -14,7 +14,7 @@ import (
 	"regexp/syntax"
 	"sort"
 
-	"github.com/google/codesearch/sparse"
+	"github.com/Hiroko103/codesearch/sparse"
 )
 
 // A matcher holds the state for running regular expression search.
